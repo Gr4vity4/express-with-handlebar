07514ba7828ce2982da850fb8283230460f31f69
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2020-02-19 23:25:55 +0700

    handle session sign-up form

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 let router = express.Router();
 
+const targetBaseUrl = "http://localhost:3000";
+
 router.use(function timeLog(req, res, next) {
   console.log("Time: ", Date.now());
   next();
@@ -13,30 +15,42 @@ router.get("/", function(req, res) {
 });
 
 router.get("/sign-up", function(req, res) {
-  res.render("sign_up");
+  const form = {
+    firstName: req.cookies.old !== undefined ? req.cookies.old.firstName : "",
+    lastName: req.cookies.old !== undefined ? req.cookies.old.lastName : "",
+    email: req.cookies.old !== undefined ? req.cookies.old.email : ""
+  };
+
+  const errors = {
+    password:
+      req.cookies.errors !== undefined ? req.cookies.errors.password : ""
+  };
+
+  res.render("sign_up", { form: form, errors: errors });
+  res.clearCookie("old");
 });
 
 /* POST Method */
 
 router.post("/sign-up", function(req, res) {
-  console.log(req.body);
+  let firstName = req.body.first_name;
+  let lastName = req.body.last_name;
+  let email = req.body.email;
+  let password = req.body.password;
+  let password_confirmation = req.body.password_confirmation;
 
-  //   let firstName = req.body.first_name;
-  //   let lastName = req.body.last_name;
-  //   let email = req.body.email;
-  //   let password = req.body.password;
-  //   let password_confirmation = req.body.password_confirmation;
-
-  //   if (password !== password_confirmation) {
-  //     res.cookie("errors", "กรุณาตรวจสอบรหัสผ่านใหม่อีกครั้ง");
-  //     res
-  //       .cookie("old", {
-  //         firstName: firstName,
-  //         lastName: lastName,
-  //         email: email
-  //       })
-  //       .redirect(200, "sign-up");
-  //   }
+  if (password !== password_confirmation) {
+    console.log(">>> passwords does not match.");
+    res.cookie("errors", {
+      password: "กรุณาตรวจสอบรหัสผ่านใหม่อีกครั้ง"
+    });
+    res.cookie("old", {
+      firstName: firstName,
+      lastName: lastName,
+      email: email
+    });
+    res.redirect(`${targetBaseUrl}/sign-up`);
+  }
 
   res.send("done.");
 });
